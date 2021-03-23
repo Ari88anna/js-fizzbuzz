332c9413b1d0a03f8ee9d18d4c016db81d478e83
Arianna Carotti <arianna.carotti@gmail.com>
2021-03-23 18:45:35 +0100

    corretto

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
-// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero 
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
 
 // crea lista numeri da 1 a 100
 
-arrayE = []
+for ( var i = 1; i <= 100; i++ ) {
+    var listaN = i;
+    
 
-for ( var i= 1; i<101; i++ ) {
-    listaN = i;
+    if (  listaN % 3==0 && listaN % 5==0 ) {
+        
+        console.log('FizzBuzz');    
+        
+    } else if ( listaN % 5==0 ) {
+        
+        console.log('buzz');
 
-    console.log(listaN);  
+    }  else if (listaN % 3==0 ) {
+        
+        console.log('Fizz');
 
+    } else {
+
+        console.log('lista num', listaN);
+    }
 }
+
+
+
+ 
